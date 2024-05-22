c258cb819e3d39b83a850eb3c9a918eba07510bd
AnnyMee <anny.petruc@gmail.com>
2024-05-22 11:27:48 +0200

    Initial commit

---
+++ src/main/java/fast_fix/FastFixApplication.java
@@ -0,0 +1,10 @@
+package fast_fix;
+
+import org.springframework.boot.SpringApplication;
+
+public class FastFixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastFixApplication.class, args);
+    }
+}
