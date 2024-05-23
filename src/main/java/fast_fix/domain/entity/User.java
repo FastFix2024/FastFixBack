9bd796b4c71e29f9ac464a67eadc7b2d459cab96
Juri Looga <looga.jury@gmail.com>
2024-05-23 10:47:17 +0200

    commit

--- src/main/java/fast_fix/domain/entity/User.java
+++ src/main/java/fast_fix/domain/entity/User.java
@@ -1,9 +1,78 @@
 package fast_fix.domain.entity;
 
+import jakarta.persistence.Entity;
+
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
 public class User {
 
     private Long id;
     private String username;
     private String password;
     private String email;
+    private Set<Role> roles;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public Set<Role> getRoles() {
+        return roles;
+    }
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (!Objects.equals(id, user.id)) return false;
+        if (!Objects.equals(username, user.username)) return false;
+        if (!Objects.equals(password, user.password)) return false;
+        if (!Objects.equals(email, user.email)) return false;
+        return Objects.equals(roles, user.roles);
+    }
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (username != null ? username.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        result = 31 * result + (roles != null ? roles.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("User: ID - %d, username - %s, password - %s, email - %s, role - %s",
+                id, username, email,password, roles);
+    }
 }
+
+
