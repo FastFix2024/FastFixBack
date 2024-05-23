920e2e2f07455b4926cb736fdd1a2c8013e20747
AnnyMee <anny.petruc@gmail.com>
2024-05-23 11:38:49 +0200

    corrected project structure

--- src/main/java/fast_fix/FastFixApplication.java
+++ src/main/java/fast_fix/FastFixApplication.java
@@ -1,11 +1,13 @@
 package fast_fix;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+@SpringBootApplication
 public class FastFixApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(FastFixApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(FastFixApplication.class, args);
+	}
+
 }
---
+++ src/test/java/fast_fix/FastFixApplicationTests.java
@@ -0,0 +1,13 @@
+package fast_fix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastFixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
