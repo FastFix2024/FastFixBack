78d5fa9356cb463f62009809f7ac5fe629b0dcec
Juri Looga <looga.jury@gmail.com>
2024-06-05 12:18:02 +0200

    coocies - AuthController

--- src/main/java/fast_fix/security/sec_controller/AuthController.java
+++ src/main/java/fast_fix/security/sec_controller/AuthController.java
@@ -5,10 +5,14 @@ import fast_fix.security.sec_dto.RefreshRequestDto;
 import fast_fix.security.sec_dto.TokenResponseDto;
 import fast_fix.security.sec_service.AuthService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Authentication controller")
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
@@ -21,22 +25,41 @@ public class AuthController {
 
     @Operation(summary = "Залогинить пользователя")
     @PostMapping("/login")
-    public ResponseEntity<Object> login(@RequestBody User user) {
+    public ResponseEntity<Object> login(@RequestBody User user, HttpServletResponse response) {
         try {
-            TokenResponseDto response = authService.login(user);
-            return ResponseEntity.ok(response);
+            TokenResponseDto responseDto = authService.login(user);
+            Cookie cookie = new Cookie("Access-Token", responseDto.getAccessToken());
+            cookie.setPath("/");
+            cookie.setHttpOnly(true);
+            response.addCookie(cookie);
+            return ResponseEntity.ok(responseDto);
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
     }
 
+    @Operation(summary = "Получить доступ")
     @PostMapping("/access")
-    public ResponseEntity<Object> getNewAccessToken(@RequestBody RefreshRequestDto request) {
+    public ResponseEntity<Object> getNewAccessToken(@RequestBody RefreshRequestDto request, HttpServletResponse response) {
         try {
-            TokenResponseDto response = authService.getAccessToken(request.getRefreshToken());
-            return ResponseEntity.ok(response);
+            TokenResponseDto responseDto = authService.getAccessToken(request.getRefreshToken());
+            Cookie cookie = new Cookie("Access-Token", responseDto.getAccessToken());
+            cookie.setPath("/");
+            cookie.setHttpOnly(true);
+            response.addCookie(cookie);
+            return ResponseEntity.ok(responseDto);
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
+    }
+
+    @Operation(summary = "Выйти из системы")
+    @GetMapping("/logout")
+    public void logout(HttpServletResponse response) {
+        Cookie cookie = new Cookie("Access-Token", null);
+        cookie.setPath("/");
+        cookie.setHttpOnly(true);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
     }
 }
